ef2e8ae87b387e790b81d6fd4fc6427b8ccbe5cf
dill-devv <kokuads@gmail.com>
2026-04-27 20:11:14 +0100

    Added console log

--- api/main.ts
+++ api/main.ts
@@ -7,9 +7,11 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
 
     if (req.method === "OPTIONS") {
         res.status(200).end('Status 200 from OPTIONS request') // do NOT put return on this line vercel tweaks out
+        console.log('200 OK')
         return;
     }
 
     res.write('get request received')
+    console.log('Get request received')
     return res.end('ending response')
 }
