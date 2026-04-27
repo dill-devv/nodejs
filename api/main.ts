69cba09b81e67f4dc347ad030fce3f60de3cfa16
dill-devv <kokuads@gmail.com>
2026-04-27 19:26:24 +0100

    Added files for server response

--- api/main.ts
+++ api/main.ts
@@ -1,1 +1,9 @@
 import {VercelRequest, VercelResponse} from '@vercel/node'
+
+export default async function handler(req: VercelRequest, res: VercelResponse) {
+    res.setHeader('Allow-Content-Allow-Methods', 'GET, OPTIONS')
+    
+    if (req.method === "OPTIONS") {res.status(200)}
+    res.write('Request received')
+    res.end()
+}
