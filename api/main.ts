066f4f980156473efefbb026dbeecff35e808bb4
dill-devv <kokuads@gmail.com>
2026-04-27 19:55:22 +0100

    Trying to fix request

--- api/main.ts
+++ api/main.ts
@@ -3,8 +3,8 @@ import {VercelRequest, VercelResponse} from '@vercel/node'
 export default async function handler(req: VercelRequest, res: VercelResponse) {
     res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS')
     
-    if (req.method === "OPTIONS") {return res.status(200).send('Options request received')}
-
-    res.write('Request received') // this shows up as response in postman
-    res.end('\nended response') // also shows up as final part of response in postman
+    if (req.method === "OPTIONS") {res.status(200).send('Options request received')} else{
+        res.write('Request received') // this shows up as response in postman
+        res.end('\nended response') // also shows up as final part of response in postman
+    }
 }
