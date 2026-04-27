b4c9e9ea2b7f590fd0766d5dabe864a341dccfe9
dill-devv <kokuads@gmail.com>
2026-04-27 21:02:12 +0100

    removed default export

--- api/hello.ts
+++ api/hello.ts
@@ -1,4 +1,4 @@
 import type {VercelRequest, VercelResponse} from '@vercel/node'
-export default async function handler(req: VercelRequest, res: VercelResponse) {
+async function handler(req: VercelRequest, res: VercelResponse) {
     return new Response('hi please work')
 }
