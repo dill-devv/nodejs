09465e1c748d9cc65dbc877a266f350fc1ba8626
dill-devv <kokuads@gmail.com>
2026-04-27 20:42:18 +0100

    stripped down response

--- api/testserve.ts
+++ api/testserve.ts
@@ -1,5 +1,3 @@
 export default async function handler(req: Request) {
-    return new Response('<p>dynamic para</p>', {
-        headers: {"Content-Type": "text/html"}
-    });
+    return new Response('hello')
 }
