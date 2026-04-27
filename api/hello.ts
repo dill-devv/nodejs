1be4bdbeba531eb48f554232c5d56ae74d8a55bc
dill-devv <kokuads@gmail.com>
2026-04-27 21:11:10 +0100

    testing page deploying

--- api/hello.ts
+++ api/hello.ts
@@ -1,4 +1,16 @@
 import type {VercelRequest, VercelResponse} from '@vercel/node'
+
+function page(title: string, content: string) {
+    return `
+        <html>
+            <body>
+                <title>${title}</title>
+                ${content}
+            </body>
+        </html>
+    `
+}
+
 export default async function handler(req: VercelRequest, res: VercelResponse) {
-    res.status(200).send('<button>hi</button>')
+    res.status(200).send(page('hi guys', '<p>hello testing para'));
 }
