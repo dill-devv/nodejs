6e9426eece7e0e6b688f58977b186fc5d3f8dea5
dill-devv <kokuads@gmail.com>
2026-04-27 19:37:20 +0100

    res.status return

--- api/main.ts
+++ api/main.ts
@@ -3,9 +3,10 @@ import {VercelRequest, VercelResponse} from '@vercel/node'
 export default async function handler(req: VercelRequest, res: VercelResponse) {
     res.setHeader('Allow-Content-Allow-Methods', 'GET, OPTIONS')
     
-    if (req.method === "OPTIONS") {res.status(200)}
-    res.write('Request received', () => {
-        console.log('req received')
+    if (req.method === "OPTIONS") {return res.status(200)}
+
+    res.write('Request received', () => { // this shows up as response in postman
+        console.log('req received') // this logs in vercel logs
     })
-    res.end('ended response')
+    res.end('ended response') // also shows up as response in postman
 }
