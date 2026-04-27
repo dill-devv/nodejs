b271cd55907b2e3a55404fe4a99fc26dc431fa03
dill-devv <kokuads@gmail.com>
2026-04-27 19:45:51 +0100

    Added message with status 200

--- api/main.ts
+++ api/main.ts
@@ -3,7 +3,7 @@ import {VercelRequest, VercelResponse} from '@vercel/node'
 export default async function handler(req: VercelRequest, res: VercelResponse) {
     res.setHeader('Allow-Content-Allow-Methods', 'GET, OPTIONS')
     
-    if (req.method === "OPTIONS") {return res.status(200)}
+    if (req.method === "OPTIONS") {return res.status(200).send('Options request received')}
 
     res.write('Request received') // this shows up as response in postman
     res.end('\nended response') // also shows up as final part of response in postman
