cf9d4d4656e1962ebaec2db86e5807ab94455fd5
dill-devv <kokuads@gmail.com>
2026-04-29 20:22:22 +0100

    ans log

--- public/src/gen.js
+++ public/src/gen.js
@@ -1,12 +1,13 @@
 let ans = Math.random(1, 100);
+console.log(ans);
 let confirm = document.getElementById('confirmGuess');
-let input = document.getElementById('input')
+let input = document.getElementById('input');
 
 confirm.addEventListener('click', function () {
-    console.log('clicked')
+    console.log('clicked');
     if (Number(input.value) != ans) {
-        console.log('wrong')
+        console.log('wrong');
     } else {
-        console.log('correct hi guys')
-    }
+        console.log('correct hi guys');
+    };
 });
